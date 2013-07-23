651d96e55a9802eaa79c72166b71cec8c6bb8e18
Theo Pack <tf.pack@googlemail.com>
2013-07-23 22:48:52 +0200

    Add simple mocks

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,21 +3,32 @@ require 'user'
 
 describe 'Twitter User' do
 
-  describe "with a username" do
+  describe 'with a username' do
     before(:each) do
       @user = User.new
       @user.twitter_username = 'furikuri'
     end
 
-    it "provides the last five tweets from Twitter" do
+    it 'provides the last five tweets from Twitter' do
+      mock_tweet1 = double('tweet1')
+      mock_tweet2 = double('tweet2')
+      mock_tweet3 = double('tweet3')
+      mock_tweet4 = double('tweet4')
+      mock_tweet5 = double('tweet5')
       tweets = [
-          "RT @cemerick: Code reuse is an absolute fucking myth.",
-          "Take the spider.io challenge. #spider_io http://t.co/XNmKQFSp",
-          "RT @unclebobmartin: Are you learning a functional language?  You'd better be!  My favorite, at the moment, is Clojure.",
-          "RT @zedshaw: RANDOM_STRING IS RANDOM: https://t.co/RWtiKrBa",
-          "RT @RomyOnRuby: I AM SO FURIOUS @ THE STORM the moment is over I am going to TATTOO THE #CLOJURE METACIRCULAR EVALUATOR on my back!! htt ..."
+          mock_tweet1,
+          mock_tweet2,
+          mock_tweet3,
+          mock_tweet4,
+          mock_tweet5
       ]
-      @user.last_five_tweets.should == tweets
+      mock_tweet1.should_receive(:text).and_return('tweet1')
+      mock_tweet2.should_receive(:text).and_return('tweet2')
+      mock_tweet3.should_receive(:text).and_return('tweet3')
+      mock_tweet4.should_receive(:text).and_return('tweet4')
+      mock_tweet5.should_receive(:text).and_return('tweet5')
+      Twitter.should_receive(:user_timeline).with('furikuri').and_return(tweets)
+      @user.last_five_tweets.should == %w(tweet1 tweet2 tweet3 tweet4 tweet5)
     end
   end
 end
